4309ca4935f93cadc033426146d50bbf5af277b0
davikir <fdl.mail1@gmail.com>
2021-10-25 22:53:20 +0300

    поправил нейминг константы

--- src/Lesson7/Main.java
+++ src/Lesson7/Main.java
@@ -3,15 +3,15 @@ package Lesson7;
 import java.util.Random;
 
 public class Main {
-  static final String[] catsName = {"Barsik", "Mursik", "Boris", "Tosik", "Bobik"};
+  static final String[] CAT_NAMES = {"Barsik", "Mursik", "Boris", "Tosik", "Bobik"};
   static Random random = new Random();
 
   public static void main(String[] args) {
     Plate plate = new Plate(15);
-    Cat[] cats = new Cat[catsName.length];
+    Cat[] cats = new Cat[CAT_NAMES.length];
 
     for (int i = 0; i < cats.length; i++) {
-      cats[i] = new Cat(catsName[i], random.nextInt(10) + 1);
+      cats[i] = new Cat(CAT_NAMES[i], random.nextInt(10) + 1);
       System.out.print("Родился ");
       cats[i].info();
     }
